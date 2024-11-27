59f231144ac03c1e281634714ab9fad01a9882cd
yblebon <135013954+yblebon@users.noreply.github.com>
2024-11-27 21:42:36 +0100

    Update resize-images.py

--- resize-images.py
+++ resize-images.py
@@ -19,7 +19,7 @@ print(f"Output directory: {output_dir}")
 
 for fname in os.listdir(images_dir):
       print(f"processing file: {fname}")
-      if fname.split(".")[1].lower() in ["jpeg", "jpg"]:
+      if fname.split(".")[1].lower() in ["jpeg", "jpg", "png"]:
          fpath = os.path.join(images_dir, fname)
          img = Image.open(fpath)
          print(f"File size: {img.size}")
